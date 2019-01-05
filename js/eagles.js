cdad6b95149ad5ecedcd0dfb71906d6c435f24f9
Nolan Miller <nolanrm20@gmail.com>
2019-01-05 14:56:30 -0700

    Add 2017 stats
    Add second API call for 2017 and added to table. Added a function to
    store the data received.

--- js/eagles.js
+++ js/eagles.js
@@ -1,40 +1,55 @@
 // This is an example of how we'd use AJAX to update the page
 // the drop down will call this function and update the div
-
 var position;
+
+// I think we should use a hash table to store the stats
+// this will make it easier to save after making two calls to API
+var statsHash = {};
+
+var teamName;
+var teamCity;
+
 function updateStats(){
 
-	// this assigns "position" to an array with all options in "select1"
-	position = document.getElementById("select1");
-	// this well set "position" to the text of the selected item in your drop down list
+	// this will set "position" to the text of the selected item in your drop down list
 	position = select1.options[select1.selectedIndex].innerHTML;
 	// then we'll call another function to make a call to an API using that position
-	sendRequest();
+	sendRequest1();
+	sendRequest2();
 
 }
 
 // here we send a request to the stats API
-function sendRequest(){
+function sendRequest1(){
 
-	const request = new XMLHttpRequest();
-	request.onreadystatechange = function() {handleResponse(request, position);};
+	const request1 = new XMLHttpRequest();
+	request1.onreadystatechange = function() {handleResponse(request1, 18);};
 	// call to api
-	request.open("GET", "https://api.mysportsfeeds.com/v2.0/pull/nfl/2018-regular/team_stats_totals.json?team=phi", true);
+	request1.open("GET", "https://api.mysportsfeeds.com/v2.0/pull/nfl/2018-regular/team_stats_totals.json?team=phi", true);
 	// authorization code (64 bit encoded)
-	request.setRequestHeader("Authorization","Basic MDE1YWViOGMtMDJkYS00MDQ1LTljYzMtMjM4YjJmOk1ZU1BPUlRTRkVFRFM=");
-  	request.send();
+	request1.setRequestHeader("Authorization","Basic MDE1YWViOGMtMDJkYS00MDQ1LTljYzMtMjM4YjJmOk1ZU1BPUlRTRkVFRFM=");
+	request1.send();
+}
+function sendRequest2(){
+
+	const request2 = new XMLHttpRequest();
+	request2.onreadystatechange = function() {handleResponse(request2, 17);};
+	// call to api
+	request2.open("GET", "https://api.mysportsfeeds.com/v2.0/pull/nfl/2017-2018-regular/team_stats_totals.json?team=phi", true);
+	// authorization code (64 bit encoded)
+	request2.setRequestHeader("Authorization","Basic MDE1YWViOGMtMDJkYS00MDQ1LTljYzMtMjM4YjJmOk1ZU1BPUlRTRkVFRFM=");
+	request2.send();
 }
 
-
 // here we handle the response
-function handleResponse(request){
+function handleResponse(request, season){
 	// http requests status codes
 	// 0 = unsent, 1 = opened, 2 = headers recieved, 3 = loading, 4 = done
 	if(request.readyState == 4) {
 		if(request.status == 200){
 			// create an object that has the response stored in it (from JSON data)
 			var teamStats = JSON.parse(request.responseText);
-			parseStats(teamStats);	
+			storeStats(teamStats, season);
 		}
 		else{
 			console.log("ERROR: " + request.statusText);
@@ -42,96 +57,127 @@ function handleResponse(request){
 	}
 }
 
+// parameter is just stats from "handleResponse()"
+function storeStats(st, season){
+	if(season == 17){
+		statsHash['Season17'] = {};
+		statsHash.Season17 = st.teamStatsTotals[0].stats;
+	}
+	else{
+		statsHash['Season18'] = {};
+		statsHash.Season18 = st.teamStatsTotals[0].stats;
+		teamName = st.teamStatsTotals[0].team.name;
+		teamCity = st.teamStatsTotals[0].team.city;
+	}
+
+	if(statsHash.hasOwnProperty('Season18') && statsHash.hasOwnProperty('Season17')){
+		writeStats();
+	}
+}
+
 // here we parse through the json data we recieved, and store the stats we want to use
-function parseStats(JSONStats){
-	// saving teamName and teamCity from JSON
-	var stats = JSONStats.teamStatsTotals[0].stats;
-	var teamName = JSONStats.teamStatsTotals[0].team.name;
-	var teamCity = JSONStats.teamStatsTotals[0].team.city;
-	var gamesPlayed = JSONStats.teamStatsTotals[0].stats.gamesPlayed;
-	
+function writeStats(){
+
 	console.log(teamCity + " " + teamName);
 
 	// adding elements recieved to the table
 	if(position == "QB"){
 
 		// pass stats
-		var passGrossYards = stats.passing.passGrossYards;
-		var passAttempts = stats.passing.passAttempts;
-		var passCompletions = stats.passing.passCompletions;
-		var passTDs = stats.passing.passTD;
-		var passPct = stats.passing.passPct;
-		var passYardsPerAtt = stats.passing.passYardsPerAtt;
-		var passInt = stats.passing.passInt;
-		var passSacks = stats.passing.passSacks;
+		var passGrossYards18 = statsHash.Season18.passing.passGrossYards;
+		var passAttempts18 = statsHash.Season18.passing.passAttempts;
+		var passCompletions18 = statsHash.Season18.passing.passCompletions;
+		var passTDs18 = statsHash.Season18.passing.passTD;
+		var passPct18 = statsHash.Season18.passing.passPct;
+		var passYardsPerAtt18 = statsHash.Season18.passing.passYardsPerAtt;
+		var passInt18 = statsHash.Season18.passing.passInt;
+		var passSacks18 = statsHash.Season18.passing.passSacks;
+		var passGrossYards17 = statsHash.Season17.passing.passGrossYards;
+		var passAttempts17 = statsHash.Season17.passing.passAttempts;
+		var passCompletions17 = statsHash.Season17.passing.passCompletions;
+		var passTDs17 = statsHash.Season17.passing.passTD;
+		var passPct17 = statsHash.Season17.passing.passPct;
+		var passYardsPerAtt17 = statsHash.Season17.passing.passYardsPerAtt;
+		var passInt17 = statsHash.Season17.passing.passInt;
+		var passSacks17 = statsHash.Season17.passing.passSacks;
 
-
-		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>" 
-												+ "</tr><tr><td>Passing Yards" + "</td><td>"
-												+ "</td><td>" + passGrossYards + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Passing Attempts" + "</td><td>"
-												+ "</td><td>" + passAttempts + "</td><td>Chart Here"
-												+ "</tr><tr><td>Passing Completions" + "</td><td>"
-												+ "</td><td>" + passCompletions + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Passing TDs" + "</td><td>"
-												+ "</td><td>" + passTDs + "</td><td>Chart Here"
-												+ "</tr><tr><td>Pass Percentage" + "</td><td>" 
-												+ "</td><td>" + passPct + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Pass Yards Per Attempt" + "</td><td>" 
-												+ "</td><td>" + passYardsPerAtt + "</td><td>Chart Here"
-												+ "</tr><tr><td>Interceptions" + "</td><td>" 
-												+ "</td><td>" + passInt + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Quarterback Sacked" + "</td><td>" 
-												+ "</td><td>" + passSacks + "</td><td>Chart Here" + "</td></table>"
+		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>"
+													+ "</tr><tr><td>Passing Yards" + "</td><td>" + passGrossYards17
+													+ "</td><td>" + passGrossYards18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Passing Attempts" + "</td><td>" + passAttempts17
+													+ "</td><td>" + passAttempts18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Passing Completions" + "</td><td>" + passCompletions17
+													+ "</td><td>" + passCompletions18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Passing TDs" + "</td><td>" + passTDs17
+													+ "</td><td>" + passTDs18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Pass Percentage" + "</td><td>" + passPct17
+													+ "</td><td>" + passPct18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Pass Yards Per Attempt" + "</td><td>" + passYardsPerAtt17
+													+ "</td><td>" + passYardsPerAtt18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Interceptions" + "</td><td>" + passInt17
+													+ "</td><td>" + passInt18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Quarterback Sacked" + "</td><td>" + passSacks17
+													+ "</td><td>" + passSacks18 + "</td><td>Chart Here" + "</td></table>"
 	}
 	else if(position == "RB"){
 
 		// rush stats
-		var rushAttempts = stats.rushing.rushAttempts;
-		var rushYards = stats.rushing.rushYards;
-		var rushTDs = stats.rushing.rushTD;
-		var rushAverage = stats.rushing.rushAverage;
-		var rush1stDowns = stats.rushing.rush1stDowns;
-		var rushFumbles = stats.rushing.rushFumbles;
+		var rushAttempts18 = statsHash.Season18.rushing.rushAttempts;
+		var rushYards18 = statsHash.Season18.rushing.rushYards;
+		var rushTDs18 = statsHash.Season18.rushing.rushTD;
+		var rushAverage18 = statsHash.Season18.rushing.rushAverage;
+		var rush1stDowns18 = statsHash.Season18.rushing.rush1stDowns;
+		var rushFumbles18 = statsHash.Season18.rushing.rushFumbles;
+		var rushAttempts17 = statsHash.Season17.rushing.rushAttempts;
+		var rushYards17 = statsHash.Season17.rushing.rushYards;
+		var rushTDs17 = statsHash.Season17.rushing.rushTD;
+		var rushAverage17 = statsHash.Season17.rushing.rushAverage;
+		var rush1stDowns17 = statsHash.Season17.rushing.rush1stDowns;
+		var rushFumbles17 = statsHash.Season17.rushing.rushFumbles;
 
-		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>" 
-												+ "</tr><tr><td>Rushing Attempts" + "</td><td>" 
-												+ "</td><td>" + rushAttempts + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Rushing Yards" + "</td><td>" 
-												+ "</td><td>" + rushYards + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Average Yards Per Attempt" + "</td><td>" 
-												+ "</td><td>" + rushAverage + "</td><td>Chart Here"
-												+ "</tr><tr><td>Rushing TDs" + "</td><td>"
-												+ "</td><td>" + rushTDs + "</td><td>Chart Here"
-												+ "</tr><tr><td>Rushing 1st Downs" + "</td><td>" 
-												+ "</td><td>" + rush1stDowns + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Rushing Fumbles" + "</td><td>" 
-												+ "</td><td>" + rushFumbles + "</td><td>Chart Here" + "</td></table>"
+		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>"
+													+ "</tr><tr><td>Rushing Attempts" + "</td><td>" + rushAttempts17
+													+ "</td><td>" + rushAttempts18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Rushing Yards" + "</td><td>" + rushYards17
+													+ "</td><td>" + rushYards18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Average Yards Per Attempt" + "</td><td>" + rushAverage17
+													+ "</td><td>" + rushAverage18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Rushing TDs" + "</td><td>" + rushTDs17
+													+ "</td><td>" + rushTDs18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Rushing 1st Downs" + "</td><td>" + rush1stDowns17
+													+ "</td><td>" + rush1stDowns18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Rushing Fumbles" + "</td><td>" + rushFumbles17
+													+ "</td><td>" + rushFumbles18 + "</td><td>Chart Here" + "</td></table>"
 	}
 	else{
 
 		// receiving stats
-		var receptions = stats.receiving.receptions;
-		var recYards = stats.receiving.recYards;
-		var recTDs = stats.receiving.recTD;
-		var recAverage = stats.receiving.recAverage;
-		var rec1stDowns = stats.receiving.rec1stDowns;
-		var recFumbles = stats.receiving.recFumbles;
+		var receptions18 = statsHash.Season18.receiving.receptions;
+		var recYards18 = statsHash.Season18.receiving.recYards;
+		var recTDs18 = statsHash.Season18.receiving.recTD;
+		var recAverage18 = statsHash.Season18.receiving.recAverage;
+		var rec1stDowns18 = statsHash.Season18.receiving.rec1stDowns;
+		var recFumbles18 = statsHash.Season18.receiving.recFumbles;
+		var receptions17 = statsHash.Season17.receiving.receptions;
+		var recYards17 = statsHash.Season17.receiving.recYards;
+		var recTDs17 = statsHash.Season17.receiving.recTD;
+		var recAverage17 = statsHash.Season17.receiving.recAverage;
+		var rec1stDowns17 = statsHash.Season17.receiving.rec1stDowns;
+		var recFumbles17 = statsHash.Season17.receiving.recFumbles;
 
-		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>" 
-												+ "</tr><tr><td>Receptions" + "</td><td>" 
-												+ "</td><td>" + receptions + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Reception Yards" + "</td><td>" 
-												+ "</td><td>" + recYards + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Average Yards Per Reception" + "</td><td>" 
-												+ "</td><td>" + recAverage + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Reception TDs" + "</td><td>" 
-												+ "</td><td>" + recTDs + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Reception 1st Downs" + "</td><td>" 
-												+ "</td><td>" + rec1stDowns + "</td><td>Chart Here" 
-												+ "</tr><tr><td>Reception Fumbles" + "</td><td>" 
-												+ "</td><td>" + recFumbles + "</td><td>Chart Here" + "</td></table>"
-	
+		document.getElementById("stats").innerHTML = "<br><table><tr><th>Stat Description</th><th><strong>2017</strong></th><th><strong>2018</strong></th><th>Charts</th>"
+													+ "</tr><tr><td>Receptions" + "</td><td>" + receptions17
+													+ "</td><td>" + receptions18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Reception Yards" + "</td><td>" + recYards17
+													+ "</td><td>" + recYards18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Average Yards Per Reception" + "</td><td>" + recAverage17
+													+ "</td><td>" + recAverage18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Reception TDs" + "</td><td>" + recTDs17
+													+ "</td><td>" + recTDs18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Reception 1st Downs" + "</td><td>" + rec1stDowns17
+													+ "</td><td>" + rec1stDowns18 + "</td><td>Chart Here"
+													+ "</tr><tr><td>Reception Fumbles" + "</td><td>" + recFumbles17
+													+ "</td><td>" + recFumbles18 + "</td><td>Chart Here" + "</td></table>"
 	}
 }
 
